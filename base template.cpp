6f389a9fa45503ca0b49d3d3ec2f69efe234dda3
sayantanpal9 <sayantanpal2006.24@kgpian.iitkgp.ac.in>
2026-08-19 18:50:44 +0530

    Add random number generator initialization
    Added a 64-bit random number generator using mt19937_64.

--- base template.cpp
+++ base template.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 #define ll long long int
 using namespace std;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+// this is a 64 bit random number generator, use rng() to use this
 int main(){
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
